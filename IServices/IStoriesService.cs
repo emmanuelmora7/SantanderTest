bb08a025296f369f3ed421c025eca3216cc53238
emmanuelmora7 <149803494+emmanuelmora7@users.noreply.github.com>
2024-08-01 11:33:45 -0600

    Add files via upload

---
+++ IServices/IStoriesService.cs
@@ -0,0 +1,8 @@
+﻿using SantanderCodeTest.Models;
+namespace SantanderCodeTest.IServices
+{
+	public interface IStoriesService
+	{
+		Task<IEnumerable<Story>?> GetBestStories(int count, CancellationToken cancellationToken);
+	}
+}
